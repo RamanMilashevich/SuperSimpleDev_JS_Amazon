d055bd4612182bab0abec1fa8b749facae6bf8cc
RamanMilashevich <roma.milashevich@gmail.com>
2024-06-24 16:32:53 +0200

    homework-14e

--- data/cart.js
+++ data/cart.js
@@ -59,3 +59,12 @@ export function addToCart(productId) {
   cart = newCart;
   saveToStorage();
 };
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => { 
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+};
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 // import { cart as myCart } from "../data/cart.js"; // as - take the input and rename it to myCart
 
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -64,16 +64,12 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuaantity() {
-  let cartQuantity = 0; // Accumulator в котором мы храним общее количество товаров для нашей корзины
-  cart.forEach((cartItem) => { 
-    cartQuantity += cartItem.quantity;
-  });
+const cartQuantity = calculateCartQuantity();
 
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity; 
+document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity; 
     
-}
+
 updateCartQuaantity();
 
 const addedMessagTimeouts = {};
